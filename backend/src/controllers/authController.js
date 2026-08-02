4f72bb9942a30da99ec31783f255e7e58918160e
Sajjadecoder <sajjadahmed102005@gmail.com>
2026-08-02 22:57:00 +0500

    modified authController to use global error handler

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,38 +1,35 @@
-import { registerUser,loginUser,getMe } from "../services/authService.js";
+import {
+  registerUser,
+  loginUser,
+  getMe,
+} from "../services/authService.js";
 
-export const register = async (req, res) => {
+export const register = async (req, res, next) => {
   try {
     const result = await registerUser(req.body);
 
     return res.status(201).json(result);
   } catch (error) {
-    return res.status(error.status || 500).json({
-      success: false,
-      message: error.message,
-    });
+    next(error);
   }
 };
-export const login = async (req, res) => {
+
+export const login = async (req, res, next) => {
   try {
     const result = await loginUser(req.body);
 
     return res.status(200).json(result);
   } catch (error) {
-    return res.status(error.status || 500).json({
-      success: false,
-      message: error.message,
-    });
+    next(error);
   }
 };
-export const getCurrentUser = async (req, res) => {
+
+export const getCurrentUser = async (req, res, next) => {
   try {
     const result = await getMe(req.user.id);
 
     return res.status(200).json(result);
   } catch (error) {
-    return res.status(error.status || 500).json({
-      success: false,
-      message: error.message,
-    });
+    next(error);
   }
 };
